05be72c900e2892782b8cf702b9478ff9aff3306
faithwei <faithwei@gmail.com>
2016-05-06 23:35:30 -0700

    lec10ex2

---
+++ lec10ex2.py
@@ -0,0 +1,29 @@
+#1
+# index = 1
+# while(index <= 20):
+# 	print index
+# 	index += 1
+
+#2
+# index = 1
+# while (index <=20):
+# 	if index == 13:
+# 		print "hello"
+# 	else:
+# 		print index
+# 	index += 1
+
+# 3 
+
+# index = 0
+# while (index <= 100):
+# 	print index
+# 	index += 10
+
+#4
+
+fruits = ["apples","oranges","bananas"]
+index = 0
+while (index < (len(fruits))):
+	print fruits[index]
+	index +=1
