669347ecbfb0087306f30a581c71c3370e6f8a3b
faithwei <faithwei@gmail.com>
2016-05-06 23:47:43 -0700

    lec9ex1

---
+++ lec9ex1.py
@@ -0,0 +1,25 @@
+# my_list = [1,2,"kitten",4,"five"]
+
+# #1
+# print my_list[2]
+# #2
+# print my_list[3]
+# #3
+# print my_list[1]
+# #4
+# print my_list[4]
+# print my_list[-1]
+# print my_list[(len(my_list))-1]
+# #5
+# print len(my_list)
+
+#6
+my_list = [1,2,"kitten",4,"five",6,7]
+
+print my_list[0]
+print my_list[1:3]
+print my_list[1:6]
+print my_list[1:10]
+print my_list[-1]
+print my_list[-1,-3]
+print my_list[-3:-1]
