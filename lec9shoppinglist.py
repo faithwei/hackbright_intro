cfc81340ad13f875ed84447bdd73ba616f9bac3d
faithwei <faithwei@gmail.com>
2016-05-07 22:43:49 -0700

    lec 9 extra exercise

--- lec9shoppinglist.py
+++ lec9shoppinglist.py
@@ -12,16 +12,43 @@ def sorted_shoppinglist():
 	shoppinglist.sort()
 	return shoppinglist
 
-def remove_item_shoppinglist():
+def remove_shoppinglist(item):
 	item = item.lower()
 	if item not in shoppinglist:
 		print "%s does not exist on list." % (item)
 	else:
 		shoppinglist.remove(item)
-		print "%s updated shopping list" % (item), sorted_shoppinglist()
+		print "%s removed from shopping list. Updated shopping list:" % (item), sorted_shoppinglist()
+
+def replace_item(old_item, new_item):
+    old_item = old_item.lower()
+    new_item = new_item.lower()
+
+    if old_item in shoppinglist:
+        item_index = shoppinglist.index(old_item)
+        print "%s has replaced %s in the list." % (new_item, old_item)
+    else:
+        print "%s is not in the list." % (old_item)	
 
 
+# TEST FUNCTIONS
+# 1 - add 4 times to your shopping list
+add_to_shoppinglist("apple")
+add_to_shoppinglist("steak")
+add_to_shoppinglist("beef")
+add_to_shoppinglist("mustard")
 
+# # 2 - Add an item that is already in the list. what happens?
+add_to_shoppinglist("apple")
+
+# # 3 - Remove an item on your list
+remove_shoppinglist("apple")
+
+# # 4 - Remove an item that is not in the list. what happens?
+remove_shoppinglist("chicken")
+
+# 5 - you've changed your mind on one of your items. you want to substitute it with something else.
+replace_item("mustard", "ketchup")
 
 
 
