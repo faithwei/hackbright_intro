a0d5879b7c089bac8abdb3cfb70e1a85680326a5
faithwei <faithwei@gmail.com>
2016-05-24 21:31:46 -0700

    Add files via upload

---
+++ lec14CodeApp_addressbook.py
@@ -0,0 +1,28 @@
+#Address book
+#A list of dictionaries
+#List - Names
+#Each contact is a dictionary
+#Functions
+# -add contact
+# -add info - first name, last name, number, email, birthday
+# -show view contact
+# -change contact info
+# -delete contact
+#Start with empty contact list
+
+addressbook = []
+
+def create_contact(first_name, last_name, phone):
+	contact = {"First Name": first_name, "Last Name": last_name, "Phone": phone}
+
+def add_contact(info):
+	
+
+def main():
+
+	first = "Faith"
+	last = "Ng"
+	phone = "4151234567"
+
+	create_contact(first, last, phone)
+
